21638e149d31e9cf020a3a38ca5a4dda8a0c0095
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-03-10 14:07:53 +0100

    cosmetic changes

--- src/main/java/pl/coderslab/filter/EncodingFilter.java
+++ src/main/java/pl/coderslab/filter/EncodingFilter.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 
 @WebFilter("/*")
 public class EncodingFilter implements Filter {
-    public void destroy() {
-    }
+
+    public void destroy() {}
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
@@ -16,8 +16,6 @@ public class EncodingFilter implements Filter {
         chain.doFilter(req, resp);
     }
 
-    public void init(FilterConfig config) throws ServletException {
-
-    }
+    public void init(FilterConfig config) throws ServletException {}
 
 }
