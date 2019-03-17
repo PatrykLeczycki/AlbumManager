3c31176a6534d98b0fd73ff3699d7a114cc38fac
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-03-17 16:36:19 +0100

    added admin collection display action

--- src/main/java/pl/coderslab/controller/AdminController.java
+++ src/main/java/pl/coderslab/controller/AdminController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.model.LoggedUser;
 import pl.coderslab.service.UserService;
 
 import javax.servlet.http.HttpSession;
@@ -16,11 +17,20 @@ public class AdminController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private LoggedUser loggedUser;
+
     @RequestMapping("/dashboard")
     public String dashboard(HttpSession session, Model model){
 
         model.addAttribute("dashboard", true);
         return "admins/dashboard";
+    }
+
+    @GetMapping("/albums")
+    private String allUserAlbums(Model model){
+        model.addAttribute("albums", userService.getAllUserAlbums(loggedUser.getId()));
+        return "admins/allalbums";
     }
 
     @GetMapping("/users")
