2c28bbe7570dc668114465e66e45b49752d5b3dd
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-03-17 14:12:53 +0100

    init commit

---
+++ src/main/resources/META-INF/sql/data.sql
@@ -0,0 +1,15 @@
+insert into label values (null, "Poland", "Prosto");
+insert into label values (null, "Poland", "SBM Label");
+
+insert into artist values (null, "1977-03-11", "Wojciech", "Poland", "Sokół", "M", "Sosnowski");
+insert into artist values (null, "1987-12-11", "Mateusz", "Poland", "Białas", "M", "Karaś");
+insert into artist values (null, "1991-07-07", "Kuba", "Poland", "Quebonafide", "M", "Grabowski");
+insert into artist values (null, "1998-04-21", "Borys", "Poland", "Bedoes", "M", "Przybylski");
+insert into artist values (null, "1992-07-16", "Mateusz", "Poland", "Guzior", "M", "Bluza");
+insert into artist values (null, "1982-05-21", "Adam", "Poland", "Łona", "M", "Zieliński");
+insert into artist values (null, "1990-03-29", "Damian", "Poland", "Bonson", "M", "Kowalski");
+insert into artist values (null, "1984-01-25", "Tomasz", "Poland", "VNM", "M", "Lewandowski");
+insert into artist values (null, "1982-12-24", "Piotr", "Poland", "Ten Typ Mes", "M", "Szmidt");
+insert into artist values (null, "1995-08-15", "Keith", "United States", "Chief Keef", "M", "Cozart");
+insert into artist values (null, "1989-05-24", "Gerald", "United States", "G-Eazy", "M", "Gillum");
+insert into artist values (null, "1986-10-24", "Aubrey", "Canada", "Drake", "M", "Graham");
