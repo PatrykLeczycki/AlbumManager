f61bb31098c53b74de702e8f55253d4c5dea72eb
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-03-30 17:00:54 +0100

    unnecessary getter/setter code deleted

--- src/main/java/pl/coderslab/model/Album.java
+++ src/main/java/pl/coderslab/model/Album.java
@@ -48,62 +48,6 @@ public class Album {
     @NotNull
     private String genre;
 
-    /*public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Label getLabel() {
-        return label;
-    }
-
-    public void setLabel(Label label) {
-        this.label = label;
-    }
-
-    public LocalDate getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(LocalDate releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public Format getFormat() {
-        return format;
-    }
-
-    public void setFormat(Format format) {
-        this.format = format;
-    }
-
-    public List<Artist> getArtists() {
-        return artists;
-    }
-
-    public void setArtists(List<Artist> artists) {
-        this.artists = artists;
-    }*/
-
     public String getArtistsToString(){
         StringBuilder artists = new StringBuilder();
         int counter = 0;
