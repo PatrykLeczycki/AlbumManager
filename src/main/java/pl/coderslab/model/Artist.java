daee79bbf7e48aecad6797cf9b25bb9deedf2cfa
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-03-30 17:01:04 +0100

    unnecessary getter/setter code deleted

--- src/main/java/pl/coderslab/model/Artist.java
+++ src/main/java/pl/coderslab/model/Artist.java
@@ -40,62 +40,6 @@ public class Artist {
     @NotNull
     private String nationality;
 
-    /*public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPseudonym() {
-        return pseudonym;
-    }
-
-    public void setPseudonym(String pseudonym) {
-        this.pseudonym = pseudonym;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }*/
-
     public Integer getAge() {
         return Period.between(birthDate, LocalDate.now()).getYears();
     }
