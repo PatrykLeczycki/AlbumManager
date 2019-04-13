2d9a268c835591cf899c5b83d52ae030d598e0e5
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-04-13 21:37:47 +0200

    rename changes

--- src/main/java/pl/coderslab/service/LabelService.java
+++ src/main/java/pl/coderslab/service/LabelService.java
@@ -3,7 +3,7 @@ package pl.coderslab.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.coderslab.model.Label;
-import pl.coderslab.repository.LabelRepo;
+import pl.coderslab.repository.LabelRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,21 +12,21 @@ import java.util.Optional;
 public class LabelService {
 
     @Autowired
-    private LabelRepo labelRepo;
+    private LabelRepository labelRepository;
 
     public void addLabel(Label label){
-        labelRepo.save(label);
+        labelRepository.save(label);
     }
 
     public List<Label> getAllLabels(){
-        return labelRepo.findAll();
+        return labelRepository.findAll();
     }
 
     public Optional<Label> getLabelById(long id){
-        return labelRepo.findById(id);
+        return labelRepository.findById(id);
     }
 
     public void deleteLabel(long id){
-        labelRepo.deleteById(id);
+        labelRepository.deleteById(id);
     }
 }
