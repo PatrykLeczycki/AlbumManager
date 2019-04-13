0957f86c3c987b17be255266870d320348900b79
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-04-13 21:37:42 +0200

    rename changes

--- src/main/java/pl/coderslab/repository/LabelRepository.java
+++ src/main/java/pl/coderslab/repository/LabelRepository.java
@@ -3,5 +3,5 @@ package pl.coderslab.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.coderslab.model.Label;
 
-public interface LabelRepo extends JpaRepository <Label, Long> {
+public interface LabelRepository extends JpaRepository <Label, Long> {
 }
