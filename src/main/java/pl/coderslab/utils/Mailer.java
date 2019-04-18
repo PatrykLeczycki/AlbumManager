187b2e8b4c3d30b36d7fe5897ece8a36b7098c7f
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-04-18 14:55:21 +0200

    corrected mail config

--- src/main/java/pl/coderslab/utils/Mailer.java
+++ src/main/java/pl/coderslab/utils/Mailer.java
@@ -14,14 +14,12 @@ public class Mailer {
         String from = Functions.getData()[0];
         String pass = Functions.getData()[1];
 
-        //Get properties object
         Properties props = new Properties();
+        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.starttls.enable", true);
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.port", "587");
+
         //get Session
         Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
@@ -36,7 +34,6 @@ public class Mailer {
             message.setSubject(sub);
 //            message.setText(msg); // send email without format html
             message.setContent(msg, "text/html");
-            //send message
             Transport.send(message);
             System.out.println("message sent successfully");
         } catch (MessagingException e) {
