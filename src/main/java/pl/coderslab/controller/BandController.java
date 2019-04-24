3ab9b1d9115f0fa85df1197cd7177b519de972e0
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-04-25 01:41:30 +0200

    added action displaying all bands

--- src/main/java/pl/coderslab/controller/BandController.java
+++ src/main/java/pl/coderslab/controller/BandController.java
@@ -2,10 +2,22 @@ package pl.coderslab.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.service.BandService;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/bands")
 public class BandController {
+
+    private final BandService bandService;
+
+    @GetMapping("/all")
+    private String allBands(Model model){
+        System.out.println(bandService.getAllBands().size());
+        model.addAttribute("bands", bandService.getAllBands());
+        return "bands/all";
+    }
 }
