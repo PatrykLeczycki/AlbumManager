4050e923079403435ea6f6a266570fe125d60fc3
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-04-24 18:41:41 +0200

    band entity modified

--- src/main/java/pl/coderslab/model/Band.java
+++ src/main/java/pl/coderslab/model/Band.java
@@ -1,12 +1,30 @@
 package pl.coderslab.model;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@Entity
+@Data
+@RequiredArgsConstructor
 public class Band {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JoinTable(name = "band_artist", joinColumns = @JoinColumn(name = "band_id"), inverseJoinColumns = @JoinColumn(name = "artist_id"))
     private List<Artist> members;
-    private LocalDate formed;
+
+    private LocalDate formDate;
 
 }
