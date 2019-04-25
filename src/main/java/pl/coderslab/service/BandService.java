4a1fe62c45b2f4c1e59a13470e566094c26c83e2
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-04-25 16:00:05 +0200

    checking if band is optional is moved to service

--- src/main/java/pl/coderslab/service/BandService.java
+++ src/main/java/pl/coderslab/service/BandService.java
@@ -2,10 +2,13 @@ package pl.coderslab.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.coderslab.model.Artist;
 import pl.coderslab.model.Band;
 import pl.coderslab.repository.BandRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +22,29 @@ public class BandService {
 
     public List<Band> getAllBands(){
         return bandRepository.findAll();
+    }
+
+    public Band getBandById(Long id){
+        Optional<Band> optionalBand = bandRepository.findById(id);
+
+        return optionalBand.orElse(null);
+    }
+
+    public List<Band> getBandsByArtist(Artist artist){
+
+        List<Band> allBands = getAllBands();
+
+        List<Band> bandsByArtist = new ArrayList<>();
+
+        for (Band band : allBands){
+            if (band.getMembers().contains(artist))
+                bandsByArtist.add(band);
+        }
+
+        return bandsByArtist;
+    }
+
+    public void deleteBand(Long id){
+        bandRepository.deleteById(id);
     }
 }
