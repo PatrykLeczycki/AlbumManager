2a419f7c9be8676c96d354a6e44d2d06df6560ce
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-04-25 15:56:55 +0200

    now admin can't delete object from database if other object is associated with it

--- src/main/java/pl/coderslab/repository/AlbumRepository.java
+++ src/main/java/pl/coderslab/repository/AlbumRepository.java
@@ -4,10 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import pl.coderslab.model.Album;
+import pl.coderslab.model.Artist;
+import pl.coderslab.model.Band;
+import pl.coderslab.model.Label;
 
 import java.util.List;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+    List<Album> findAlbumsByLabel(Label label);
+    List<Album> findAlbumsByBand(Band band);
 
     @Query(value = "select user_album.album_id from user_album where User_id = :tempid", nativeQuery = true)
     List<Long> getAlbumIdsByUserId(@Param("tempid") Long id);
