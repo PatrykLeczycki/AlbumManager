f008317cb5f30da0d032162cddd1f624696d0095
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-05-08 15:15:57 +0200

    prompts displaying improved

--- src/main/java/pl/coderslab/controller/ArtistController.java
+++ src/main/java/pl/coderslab/controller/ArtistController.java
@@ -1,22 +1,40 @@
 package pl.coderslab.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.service.ArtistService;
+import pl.coderslab.utils.Prompt;
 
 
 @Controller
 @RequestMapping("/artists")
+@RequiredArgsConstructor
 public class ArtistController {
 
-    @Autowired
-    private ArtistService artistService;
+    private final ArtistService artistService;
+    private final Prompt prompt;
 
     @GetMapping("/all")
     private String allArtists(Model model){
         model.addAttribute("artists", artistService.getAllArtists());
+
+        if(prompt.doesContain("deleteerror")){
+            prompt.getNames().remove("deleteerror");
+            model.addAttribute("deleteerror", true);
+        }
+
+        if(prompt.doesContain("deleteerror2")){
+            prompt.getNames().remove("deleteerror2");
+            model.addAttribute("deleteerror2", true);
+        }
+
+        if(prompt.doesContain("artistnotfound")){
+            prompt.getNames().remove("artistnotfound");
+            model.addAttribute("artistnotfound", true);
+        }
+
         return "artists/all";
     }
 }
