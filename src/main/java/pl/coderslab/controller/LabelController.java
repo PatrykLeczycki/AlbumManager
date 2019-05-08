6d31de327cef46adc71d097a12da070120ed85ef
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-05-08 15:16:08 +0200

    prompts displaying improved

--- src/main/java/pl/coderslab/controller/LabelController.java
+++ src/main/java/pl/coderslab/controller/LabelController.java
@@ -1,21 +1,34 @@
 package pl.coderslab.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.service.LabelService;
+import pl.coderslab.utils.Prompt;
 
 @Controller
 @RequestMapping("/labels")
+@RequiredArgsConstructor
 public class LabelController {
 
-    @Autowired
-    private LabelService labelService;
+    private final LabelService labelService;
+    private final Prompt prompt;
 
     @GetMapping("/all")
     public String allLabels(Model model){
         model.addAttribute("labels", labelService.getAllLabels());
+
+        if(prompt.doesContain("deleteerror")){
+            prompt.getNames().remove("deleteerror");
+            model.addAttribute("deleteerror", true);
+        }
+
+        if(prompt.doesContain("labelnotfound")){
+            prompt.getNames().remove("labelnotfound");
+            model.addAttribute("labelnotfound", true);
+        }
+
         return "labels/all";
     }
 }
