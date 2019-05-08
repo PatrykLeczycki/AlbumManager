5c9d057d38722419d251bfedea8623dc2f4e6f5e
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-05-08 15:16:03 +0200

    prompts displaying improved

--- src/main/java/pl/coderslab/controller/BandController.java
+++ src/main/java/pl/coderslab/controller/BandController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.service.BandService;
+import pl.coderslab.utils.Prompt;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,10 +14,22 @@ import pl.coderslab.service.BandService;
 public class BandController {
 
     private final BandService bandService;
+    private final Prompt prompt;
 
     @GetMapping("/all")
     private String allBands(Model model){
         model.addAttribute("bands", bandService.getAllBands());
+
+        if(prompt.doesContain("deleteerror")){
+            prompt.getNames().remove("deleteerror");
+            model.addAttribute("deleteerror", true);
+        }
+
+        if(prompt.doesContain("bandnotfound")){
+            prompt.getNames().remove("bandnotfound");
+            model.addAttribute("bandnotfound", true);
+        }
+
         return "bands/all";
     }
 }
