57a439b4acf8c455bf77531e76eedbad4d99cdd7
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-06-05 16:39:34 +0200

    corrected urls

--- src/main/java/pl/coderslab/service/UserService.java
+++ src/main/java/pl/coderslab/service/UserService.java
@@ -103,7 +103,7 @@ public class UserService {
 
         User user = userRepo.findUserByEmail(receiverEmail);
 
-        String link = "http://77.55.213.198:8080/AlbumManager-1.0-SNAPSHOT/register/" + user.getId() + "/" + user.getRegistrationToken();
+        String link = "pleczycki.pl/albummanager/register/" + user.getId() + "/" + user.getRegistrationToken();
 
         //String link = "http://localhost:8080/register/" + user.getId() + "/" + user.getRegistrationToken();
 
@@ -113,7 +113,7 @@ public class UserService {
                 "<title>Confirm registration</title>" +
                 "</head>" +
                 "<body>" +
-                "<<div style=\"background-color: #f9c910; text-align: center\"><h1><a href=\"http://77.55.213.198:8080/AlbumManager-1.0-SNAPSHOT/\">Album Manager</a></h1></div>" +
+                "<<div style=\"background-color: #f9c910; text-align: center\"><h1><a href=\"pleczycki.pl/albummanager/\">Album Manager</a></h1></div>" +
                 "<p style=\"text-align: center\">Thank you for registering on our site. Please click link below to confirm registration process:</p>" +
                 "<p style=\"text-align: center\"><a style=\"color: #2c7021; text-decoration: none; font-size: 30px\" href=\"" + link + "\" target=\"_blank\">Confirm registration</a></p>" +
                 "<p style=\"margin-top: 50px; text-align: center\">If you didn't register on our site, ignore this e-mail and make sure your data is safe.</p>" +
@@ -126,9 +126,9 @@ public class UserService {
 
         User user = userRepo.findUserByEmail(receiverEmail);
 
-        //String link = "http://77.55.213.198:8080/AlbumManager-1.0-SNAPSHOT/lostpassword/" + user.getId() + "/" + user.getPassRecoveryToken();
+        String link = "pleczycki.pl/albummanager/lostpassword/" + user.getId() + "/" + user.getPassRecoveryToken();
 
-        String link = "http://localhost:8080/lostpassword/" + user.getId() + "/" + user.getPassRecoveryToken();
+        //String link = "http://localhost:8080/lostpassword/" + user.getId() + "/" + user.getPassRecoveryToken();
 
 
 
@@ -137,33 +137,11 @@ public class UserService {
                 "<title>Password recovery</title>" +
                 "</head>" +
                 "<body>" +
-                "<<div style=\"background-color: #f9c910; text-align: center\"><h1><a href=\"http://77.55.213.198:8080/AlbumManager-1.0-SNAPSHOT/\">Album Manager</a></h1></div>" +
+                "<<div style=\"background-color: #f9c910; text-align: center\"><h1><a href=\"pleczycki.pl/albummanager/\">Album Manager</a></h1></div>" +
                 "<p style=\"text-align: center\">You have received this e-mail because your e-mail address was given during password retrieval process. To retrieve your password click here:</p>" +
                 "<p style=\"text-align: center\"><a style=\"color: #2c7021; text-decoration: none; font-size: 30px\" href=\"" + link + "\" target=\"_blank\">Retrieve password</a></p>" +
-                "<p style=\"margin-top: 50px; text-align: center\">If you didn't lost your password, ignore this e-mail and make sure your data is safe.</p>" +
-                "<p style=\"margin-top: 50px; text-align: center\">Pozdrawiamy</p>" +
-                "<p style=\"text-align: center\"><a href=\"http://www.ameliaweb.pl/gifts\">Link do aplikacji.</a></p>" +
-                "</body></html>";
+                "<p style=\"margin-top: 50px; text-align: center\">If you didn't lost your password, ignore this e-mail and make sure your data is safe.</p>" + "</body></html>";
 
         mailer.send(receiverEmail, "Album Manager - password recovery", message);
     }
-
-
-    /*
-
-    <html lang="en">
-<head>
-    <title>Title</title>
-</head>
-<body>
-<div style="background-color: #f9c910; text-align: center"><h1>Album Manager</h1></div>
-<p style="text-align: center">You have received this e-mail because your e-mail address was given during password retrieval process. To retrieve your password click here:</p>
-<p style="text-align: center"><a style="color: #2c7021; text-decoration: none; font-size: 30px" href="http://77.55.213.198:8080/AlbumManager-1.0-SNAPSHOT/" target="_blank">Retrieve password</a></p>
-<p style="margin-top: 50px; text-align: center">If you didn't lost your password, ignore this e-mail and make sure your data is safe.</p>
-</body>
-</html>
-
-     */
-
-
 }
